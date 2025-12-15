01df055782b825c58dfe1f56279bd450cf5fe00c
Fathima Salma Muzammil <salmamuzam77@gmail.com>
2025-12-15 16:20:45 +0530

    Updated the App and Guest Layout File. Designed the Navbar and Footer. Refactored the Code.

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0 dark:bg-gray-900">
+<div class="flex flex-col items-center w-full min-h-screen pt-6 sm:justify-center sm:pt-0">
     <div>
         {{ $logo }}
     </div>
