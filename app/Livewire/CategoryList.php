4e762619a6a5030da32561a899b048e0ba1beed5
Fathima Salma Muzammil <salmamuzam77@gmail.com>
2025-12-19 08:14:22 +0530

    Delete Functionality

--- app/Livewire/CategoryList.php
+++ app/Livewire/CategoryList.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Category;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class CategoryList extends Component
@@ -21,7 +22,30 @@ class CategoryList extends Component
 
     // Delete functionality
 
-    public function deleteCategory(Category $category){
-        dd($category);
+    public function deleteCategory(Category $category)
+    {
+        // If category is available, delete it
+        if ($category) {
+            // if the image exists, remove the image from the storage
+            if (Storage::exists($category->image)) {
+                // delete the image
+                Storage::delete($category->image);
+            }
+            ;
+            // delete the category
+            $deleteResponse = $category->delete();
+
+            if ($deleteResponse) {
+                session()->flash('success', 'Category deleted successfully!');
+            } else {
+                session()->flash('error', 'Unable to delete category, please try again!');
+            }
+        } else {
+            session()->flash('error', 'Category not found!');
+        }
+
+        // redirect to the category listing page
+        // prevent page reload
+        return $this->redirect('/categories', navigate: true);
     }
 }
