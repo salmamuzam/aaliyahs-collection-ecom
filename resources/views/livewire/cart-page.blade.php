57c1ad3286be61b1230a4072bef813653a00a74f
Fathima Salma Muzammil <salmamuzam77@gmail.com>
2025-12-24 19:36:58 +0530

    Worked on the cart page halfway

--- resources/views/livewire/cart-page.blade.php
+++ resources/views/livewire/cart-page.blade.php
@@ -5,23 +5,16 @@
 
             <div class="relative grid gap-4 mt-6 lg:grid-cols-3">
                 <div class="space-y-4 lg:col-span-2">
-                    <div class="relative p-6 bg-white border border-gray-300 rounded-md shadow-sm">
+                    @forelse($cart_items as $item)
+                    <div wire:key="{{ $item['product_id'] }}" class="relative p-6 bg-white border border-gray-300 rounded-md shadow-sm">
                         <div class="flex items-center gap-4 max-sm:flex-col max-sm:gap-6">
                             <div class="w-52 h-52 shrink-0">
-                                <img src='https://readymadeui.com/images/laptop2.webp'
-                                    class="object-contain w-full h-full" />
+                                <img src='{{ url('storage', $item['image']) }}'
+                                    class="object-contain w-full h-full" alt="{{ $item['name'] }}" />
                             </div>
                             <div class="w-full sm:border-l sm:pl-4 sm:border-gray-300">
-                                <h3 class="text-base font-semibold text-slate-900">HP ProBook 455 (15.6) 39.6 cm G9
-                                    Business Laptop</h3>
-                                <ul class="mt-4 space-y-2 text-sm font-medium text-slate-500">
-                                    <li>AMD Ryzen™ 5 processor</li>
-                                    <li>FreeDOS</li>
-                                    <li>16 GB DDR4 RAM</li>
-                                    <li>512 GB PCIe® NVMe™ SSD Hard Drive</li>
-                                    <li><a href="javascript:void(0);" class="text-blue-600">(See more
-                                            Specifications)</a></li>
-                                </ul>
+                                <h3 class="text-base font-semibold text-slate-900">
+                                    {{ $item['name'] }}</h3>
 
                                 <hr class="my-4 border-gray-300" />
 
@@ -37,7 +30,7 @@
                                                     data-original="#000000"></path>
                                             </svg>
                                         </button>
-                                        <span class="font-semibold text-base leading-[16px]">2</span>
+                                        <span class="font-semibold text-base leading-[16px]">{{ $item['quantity'] }}</span>
                                         <button type="button"
                                             class="flex items-center justify-center w-[18px] h-[18px] bg-blue-600 outline-none rounded-sm cursor-pointer">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="w-2 fill-white"
@@ -49,7 +42,7 @@
                                         </button>
                                     </div>
                                     <div class="flex items-center">
-                                        <h4 class="text-base font-semibold text-slate-900">$1020.00</h4>
+                                        <h4 class="text-base font-semibold text-slate-900"> LKR {{ $item['unit_amount'] }}</h4>
                                         <svg xmlns="http://www.w3.org/2000/svg"
                                             class="w-3 cursor-pointer shrink-0 fill-gray-400 hover:fill-red-500 absolute top-3.5 right-3.5"
                                             viewBox="0 0 320.591 320.591">
@@ -61,71 +54,19 @@
                                                 data-original="#000000"></path>
                                         </svg>
                                     </div>
+
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="relative p-6 bg-white border border-gray-300 rounded-md shadow-sm">
-                        <div class="flex items-center gap-4 max-sm:flex-col max-sm:gap-6">
-                            <div class="w-52 h-52 shrink-0">
-                                <img src='https://readymadeui.com/images/laptop4.webp'
-                                    class="object-contain w-full h-full" />
-                            </div>
-                            <div class="w-full sm:border-l sm:pl-4 sm:border-gray-300">
-                                <h3 class="text-base font-semibold text-slate-900">ThinkPad E16 40.64cms - 13th Gen
-                                    Intel i5</h3>
-                                <ul class="mt-4 space-y-2 text-sm font-medium text-slate-500">
-                                    <li>AMD Ryzen™ 5 processor</li>
-                                    <li>FreeDOS</li>
-                                    <li>16 GB DDR4 RAM</li>
-                                    <li>512 GB PCIe® NVMe™ SSD Hard Drive</li>
-                                    <li><a href="javascript:void(0);" class="text-blue-600">(See more
-                                            Specifications)</a></li>
-                                </ul>
-
-                                <hr class="my-6 border-gray-300" />
-
-                                <div class="flex flex-wrap items-center justify-between gap-4">
-                                    <div class="flex items-center gap-4">
-                                        <h4 class="text-sm font-semibold text-slate-900">Qty:</h4>
-                                        <button type="button"
-                                            class="flex items-center justify-center w-[18px] h-[18px] bg-blue-600 outline-none rounded-sm cursor-pointer">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-2 fill-white"
-                                                viewBox="0 0 124 124">
-                                                <path
-                                                    d="M112 50H12C5.4 50 0 55.4 0 62s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12z"
-                                                    data-original="#000000"></path>
-                                            </svg>
-                                        </button>
-                                        <span class="font-semibold text-base leading-[16px]">2</span>
-                                        <button type="button"
-                                            class="flex items-center justify-center w-[18px] h-[18px] bg-blue-600 outline-none rounded-sm cursor-pointer">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-2 fill-white"
-                                                viewBox="0 0 42 42">
-                                                <path
-                                                    d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"
-                                                    data-original="#000000"></path>
-                                            </svg>
-                                        </button>
-                                    </div>
-                                    <div class="flex items-center">
-                                        <h4 class="text-base font-semibold text-slate-900">$1100.00</h4>
-                                        <svg xmlns="http://www.w3.org/2000/svg"
-                                            class="w-3 cursor-pointer shrink-0 fill-gray-400 hover:fill-red-500 absolute top-3.5 right-3.5"
-                                            viewBox="0 0 320.591 320.591">
-                                            <path
-                                                d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"
-                                                data-original="#000000"></path>
-                                            <path
-                                                d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"
-                                                data-original="#000000"></path>
-                                        </svg>
-                                    </div>
+                                <div class="mt-6">
+                                 <span class="font-semibold text-base text-red-600 leading-[16px]">Total: LKR {{ $item['total_amount'] }}</span>
+                           </div>
                                 </div>
-                            </div>
+
                         </div>
+
                     </div>
+@empty
+@endforelse
+
                 </div>
 
                 <div class="sticky top-0 p-6 bg-white border border-gray-300 rounded-md shadow-sm h-max">
