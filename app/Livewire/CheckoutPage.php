3613aaef03469f075db1ddb58ed49398147b1910
Fathima Salma Muzammil <salmamuzam77@gmail.com>
2025-12-25 09:08:35 +0530

    Checkout Form Validation

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -5,10 +5,33 @@ namespace App\Livewire;
 use App\Helpers\CartManagement;
 use Livewire\Attributes\Title;
 use Livewire\Component;
-
+#[Title('Checkout | Aaliyah Collection')]
 class CheckoutPage extends Component
 {
-    #[Title('Checkout | Aaliyah Collection')]
+    public $first_name;
+    public $last_name;
+    public $email;
+    public $phone;
+    public $address;
+    public $city;
+    public $state;
+    public $zip;
+    public $payment_method;
+
+    public function placeOrder(){
+        $this->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+            'state' => 'required',
+            'zip' => 'required',
+            'payment_method' => 'required'
+        ]);
+    }
+
     public function render()
     {
         // fetch items from cookie
