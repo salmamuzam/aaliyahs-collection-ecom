9ad9fffca3f9c645273a2a291829ea5f94e0ef1b
Fathima Salma Muzammil <salmamuzam77@gmail.com>
2025-12-29 21:14:10 +0530

    Make success page dynamic

--- resources/views/livewire/success-page.blade.php
+++ resources/views/livewire/success-page.blade.php
@@ -1,112 +1,48 @@
-<div class="p-4 bg-white">
-          <div class="max-w-6xl mx-auto max-lg:max-w-2xl">
-              <div>
-                  <h1 class="text-2xl font-semibold text-slate-900">Order Summary</h1>
-                  <p class="mt-3 text-sm text-slate-500">Your order has been received. Here’s a summary of your order details.</p>
-              </div>
-
-              <div class="grid mt-12 lg:grid-cols-3 gap-y-16">
-                  <div class="lg:border-r lg:border-gray-300 lg:pr-8">
-                      <div>
-                          <h3 class="mb-6 text-base font-semibold text-slate-900">Shipping Information</h3>
-                          <div class="grid sm:grid-cols-2 gap-x-4 gap-y-6">
-                              <div>
-                                  <p class="text-sm font-medium text-slate-500">Customer</p>
-                                  <p class="mt-2 text-sm font-medium text-slate-900">Alex Johnson</p>
-                              </div>
-                              <div>
-                                  <p class="text-sm font-medium text-slate-500">Shipping Method</p>
-                                  <p class="mt-2 text-sm font-medium text-slate-900">Cash on Delivery</p>
-                              </div>
-                              <div>
-                                  <p class="text-sm font-medium text-slate-500">Address</p>
-                                  <p class="mt-2 text-sm font-medium text-slate-900">425 Park Avenue, San Francisco, CA 94107</p>
-                              </div>
-                              <div>
-                                  <p class="text-sm font-medium text-slate-500">Phone</p>
-                                  <p class="mt-2 text-sm font-medium text-slate-900">(555) 123-4567</p>
-                              </div>
-                          </div>
-                      </div>
-
-                      <div class="mt-12">
-                          <h3 class="mb-6 text-base font-semibold text-slate-900">Billing details</h3>
-
-                          <ul class="mt-6 space-y-4 font-medium">
-                              <li class="flex flex-wrap gap-4 text-sm text-slate-500">Subtotal <span class="ml-auto font-semibold text-slate-900">$120.00</span></li>
-
-                              <hr class="border-gray-300" />
-                              <li class="flex flex-wrap gap-4 text-[15px]">Total <span class="ml-auto font-semibold text-slate-900">$130.00</span></li>
-                          </ul>
-
-                          <div class="mt-8 space-y-3">
-                              <button type="button" class="text-sm px-4 py-2.5 w-full font-medium tracking-wide bg-purple-600 hover:bg-purple-700 text-white rounded-md cursor-pointer">View My Order</button>
-                              <button type="button" class="text-sm px-4 py-2.5 w-full font-medium tracking-wide bg-transparent text-slate-900 border border-gray-300 rounded-md cursor-pointer">Go Back Shopping  </button>
-                          </div>
-                      </div>
-                  </div>
-
-                  <div class="space-y-4 lg:col-span-2 lg:pl-8 max-lg:-order-1">
-                      <div class="space-y-4 lg:col-span-2">
-                          <div class="grid items-center gap-4 sm:grid-cols-3">
-                              <div class="flex items-center col-span-2 gap-4">
-                                  <div class="p-2 bg-gray-100 rounded-md w-28 h-28 max-sm:w-24 max-sm:h-24 shrink-0">
-                                      <img src='https://readymadeui.com/images/product14.webp' class="object-contain w-full h-full" />
-                                  </div>
-                                  <div>
-                                      <h3 class="text-sm font-semibold sm:text-base text-slate-900">Velvet Sneaker</h3>
-                                      <div class="mt-2">
-                                          <p class="text-xs font-medium text-slate-500">Category: Abaya</p>
-                                          <p class="mt-1 text-xs font-medium text-slate-500">Qty: 1</p>
-                                      </div>
-                                  </div>
-                              </div>
-                              <div class="sm:ml-auto">
-                                  <h4 class="text-base font-semibold sm:text-lg text-slate-900">$20.00</h4>
-                              </div>
-                          </div>
-
-                          <hr class="border-gray-300" />
-
-                          <div class="grid items-center gap-4 sm:grid-cols-3">
-                              <div class="flex items-center col-span-2 gap-4">
-                                  <div class="p-2 bg-gray-100 rounded-md w-28 h-28 max-sm:w-24 max-sm:h-24 shrink-0">
-                                      <img src='https://readymadeui.com/images/watch5.webp' class="object-contain w-full h-full" />
-                                  </div>
-                                  <div>
-                                      <h3 class="text-sm font-semibold sm:text-base text-slate-900">Smart Watch Timex</h3>
-                                      <div class="mt-2">
-                                          <p class="text-xs font-medium text-slate-500">Category: Hijab</p>
-                                          <p class="mt-1 text-xs font-medium text-slate-500">Qty: 1</p>
-                                      </div>
-                                  </div>
-                              </div>
-                              <div class="sm:ml-auto">
-                                  <h4 class="text-base font-semibold sm:text-lg text-slate-900">$60.00</h4>
-                              </div>
-                          </div>
-
-                          <hr class="border-gray-300" />
-
-                          <div class="grid items-center gap-4 sm:grid-cols-3">
-                              <div class="flex items-center col-span-2 gap-4">
-                                  <div class="p-2 bg-gray-100 rounded-md w-28 h-28 max-sm:w-24 max-sm:h-24 shrink-0">
-                                      <img src='https://readymadeui.com/images/watch4.webp' class="object-contain w-full h-full" />
-                                  </div>
-                                  <div>
-                                      <h3 class="text-sm font-semibold sm:text-base text-slate-900">French Connection</h3>
-                                      <div class="mt-2">
-                                          <p class="text-xs font-medium text-slate-500">Category: Dress</p>
-                                          <p class="mt-1 text-xs font-medium text-slate-500">Qty: 1</p>
-                                      </div>
-                                  </div>
-                              </div>
-                              <div class="sm:ml-auto">
-                                  <h4 class="text-base font-semibold sm:text-lg text-slate-900">$40.00</h4>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
+<section class="py-4 md:py-6">
+    <div class="p-6 mx-auto max-w-7xl max-lg:max-w-4xl">
+        <h2 class="mb-2 text-xl font-bold font-playfair text-[#004D61] dark:text-white">THANKS FOR YOUR ORDER!</h2>
+        <p class="mb-6 text-base text-[#1A1A1A] dark:text-gray-400 md:mb-8">Your order <a href="#"
+                class="font-medium text-[#822659] hover:underline">{{ $order->id }}</a> has been received.</p>
+        <div
+            class="p-6 mb-6 space-y-4 border border-gray-300 rounded-md sm:space-y-2 bg-gray-50 dark:border-gray-700 dark:bg-gray-800 md:mb-8">
+            <dl class="items-center justify-between gap-4 sm:flex">
+                <dt class="mb-1 text-base font-bold text-[#1A1A1A] sm:mb-0 dark:text-gray-400">Date</dt>
+                <dd class="text-base font-medium text-[#1A1A1A] dark:text-white sm:text-end">
+                    {{ $order->created_at->format('d-m-Y') }}</dd>
+            </dl>
+            <dl class="items-center justify-between gap-4 sm:flex">
+                <dt class="mb-1 text-base font-bold text-[#1A1A1A] sm:mb-0 dark:text-gray-400">Payment Method</dt>
+                <dd class="text-base font-medium text-[#1A1A1A] dark:text-white sm:text-end">Cash on Delivery</dd>
+            </dl>
+            <dl class="items-center justify-between gap-4 sm:flex">
+                <dt class="mb-1 text-base font-bold text-[#1A1A1A] sm:mb-0 dark:text-gray-400">Name</dt>
+                <dd class="text-base font-medium text-[#1A1A1A] dark:text-white sm:text-end">
+                    {{ $order->address->full_name }}</dd>
+            </dl>
+            <dl class="items-center justify-between gap-4 sm:flex">
+                <dt class="mb-1 text-base font-bold text-[#1A1A1A] sm:mb-0 dark:text-gray-400">Address</dt>
+                <dd class="text-base font-medium text-[#1A1A1A] dark:text-white sm:text-end">
+                    {{ $order->address->street_address }}, {{ $order->address->city }}, {{ $order->address->province }},
+                    {{ $order->address->postal_code }}, {{ $order->address->country }}</dd>
+            </dl>
+            <dl class="items-center justify-between gap-4 sm:flex">
+                <dt class="mb-1 text-base font-bold text-[#1A1A1A] sm:mb-0 dark:text-gray-400">Phone</dt>
+                <dd class="text-base font-medium text-[#1A1A1A] dark:text-white sm:text-end">
+                    {{ $order->address->phone }}</dd>
+            </dl>
+            <dl class="items-center justify-between gap-4 sm:flex">
+                <dt class="mb-1 text-base font-bold text-[#1A1A1A] sm:mb-0 dark:text-gray-400">Total</dt>
+                <dd class="text-base font-bold text-[#822659] dark:text-white sm:text-end">LKR
+                    {{ number_format($order->grand_total, 2) }}</dd>
+            </dl>
+        </div>
+        <div class="flex gap-4 max-lg:flex-col">
+            <a wire:navigate href="/my-orders"
+                class="rounded-md px-4 py-2.5 w-full text-base font-medium tracking-wide bg-[#3E5641] hover:bg-[#324534] text-white cursor-pointer text-center">View
+                your order</a>
+            <a wire:navigate href="/shop"
+                class="rounded-md px-4 py-2.5 w-full text-base font-medium tracking-wide bg-[#e6d3dd] hover:bg-[#d9c0d1] text-[#822659] cursor-pointer text-center">Return
+                to shopping</a>
+        </div>
+    </div>
+</section>
