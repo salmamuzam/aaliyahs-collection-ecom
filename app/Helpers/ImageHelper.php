70a7b774cf3866835a97b52ed288104df836fcad
Fathima Salma Muzammil <salmamuzam77@gmail.com>
2026-01-25 19:41:11 +0530

    Restore local image support for legacy uploads

--- app/Helpers/ImageHelper.php
+++ app/Helpers/ImageHelper.php
@@ -16,16 +16,18 @@ class ImageHelper
             return asset('images/placeholder.jpg');
         }
 
-        // If it's already a full URL, return it
+        // 1. If it's already a full URL (like Google profile pics), return it
         if (filter_var($path, FILTER_VALIDATE_URL)) {
             return $path;
         }
 
-        // Cloudinary Cloud Name
-        $cloudName = 'dhpirmjdb';
+        // 2. SMART CHECK: Does this file exist in your local storage? (Old images)
+        if (file_exists(public_path('storage/' . $path))) {
+            return asset('storage/' . $path);
+        }
 
-        // Base Cloudinary URL
-        // We use 'v1' as a version placeholder
+        // 3. Otherwise, it's a Cloudinary image (New images)
+        $cloudName = 'dhpirmjdb';
         return "https://res.cloudinary.com/{$cloudName}/image/upload/v1/{$path}";
     }
 }
